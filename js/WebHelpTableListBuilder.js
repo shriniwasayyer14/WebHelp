90ff3a9349afb2b81899ef778bb6783f089aca6f
akannan <arj.shiv@gmail.com>
2015-03-24 10:14:30 -0400

    Using strict and getting rid of JSHint errors

--- js/WebHelpTableListBuilder.js
+++ js/WebHelpTableListBuilder.js
@@ -1,6 +1,7 @@
 /* globals jQuery, WebHelpTemplates */
 var TableList;
 TableList = (function () {
+	"use strict";
     function TableList(tableListOptions) {
         if (tableListOptions.element === undefined) {
             //console.error('TableList needs an element to work on');
@@ -69,7 +70,6 @@ TableList = (function () {
 		$listTemplate.addClass('tableList');
 		if (this.useSearchFilter) {
 			jQuery(this.element).append($searchListTemplate).append($listTemplate);
-			var self = this;
 			$searchListTemplate.on('keyup', this._searchFunction.bind(this));
 		} else {
 			jQuery(this.element).append($listTemplate);
@@ -80,7 +80,7 @@ TableList = (function () {
 		var $thisListItemTemplate = jQuery(WebHelpTemplates[this.listItemTemplate]);
 		if (rowData) {
 			var rowLength = rowData.length;
-			if (!rowLength || rowLength != $thisListItemTemplate.children('div').length) {
+			if (!rowLength || rowLength !== $thisListItemTemplate.children('div').length) {
 				throw new Error('Attempted to add a row without any data or with incorrect parameters');
 			}
 			for (var j = 0; j < rowLength; j++) {
@@ -98,12 +98,12 @@ TableList = (function () {
 	};
 
 	TableList.prototype.numRows = function () {
-		return jQuery(this.element).find('ul > li:not(.header)').length;
+		return jQuery(this.element).find('ul').find('li:not(.header)').length;
 	};
 
 	TableList.prototype.getData = function () {
 		/*No data binding, we have to evaluate this lazily*/
-		var listItems = jQuery(this.element).find('ul > li:not(.header)');
+		var listItems = jQuery(this.element).find('ul').find('li:not(.header)');
 		var length = listItems.length;
 		var returnArray = [];
 		for (var i = 0; i < length; i++) {
@@ -124,7 +124,7 @@ TableList = (function () {
 		var $listItems = jQuery(this.element).find('.' + jQuery(WebHelpTemplates[this.listTemplate]).attr('class')).find('li');
 		var $searchBox = jQuery(this.element).find('.' + jQuery(WebHelpTemplates[this.searchListTemplate]).attr('class')).find('input');
 		var val = '^(?=.*\\b' + $searchBox.val().trim().split(/\s+/).join('\\b)(?=.*\\b') + ').*$';
-		var reg = RegExp(val, 'i');
+		var reg = new RegExp(val, 'i');
 		var text;
 
 		$listItems.show().addClass('searchResults').filter(function () {
