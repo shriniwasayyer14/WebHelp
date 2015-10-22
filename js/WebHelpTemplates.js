d3f8ef586672634ff87937d854e5453b1365d061
Arjun Shivanand Kannan <arj.shiv@gmail.com>
2015-10-22 16:55:14 -0400

    Updating modal alignment
    Modal was previously not center aligned

--- js/WebHelpTemplates.js
+++ js/WebHelpTemplates.js
@@ -20,7 +20,7 @@ WebHelpTemplates["WebHelpConsumption"] = "<div id=\"webHelpMainContent\">\n" +
 
 WebHelpTemplates["WebHelpContent"] = "<div id=\"contentConsumptionModal\" class=\"modal\">\n" +
    "	<div class=\"modal-dialog\">\n" +
-   "		<div class=\"modal-content\" style=\"width: 800px;\">\n" +
+   "		<div class=\"modal-content\">\n" +
    "			<div class=\"modal-header ai-modal-title\">\n" +
    "				<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span>\n" +
    "				</button>\n" +
@@ -32,87 +32,99 @@ WebHelpTemplates["WebHelpContent"] = "<div id=\"contentConsumptionModal\" class=
    "			</div>\n" +
    "		</div>\n" +
    "	</div>\n" +
-   "</div>";
+   "</div>\n" +
+   "";
 
-WebHelpTemplates["WebHelpCreator"] = "\n" +
-   "<div id=\"webHelpMainContent\">\n" +
-   "	<div class=\"tabbable\">\n" +
-   "		<!-- Only required for left/right tabs -->\n" +
-   "		<ul class=\"nav nav-tabs\">\n" +
-   "			<li><a href=\"#scratchpad\" data-toggle=\"tab\" target=\"_self\">Scratchpad</a></li>\n" +
-   "			<li><a href=\"#availableSequences\" data-toggle=\"tab\"  target=\"_self\">Topics</a></li>\n" +
-   "			<li><a href=\"#addSequence\" data-toggle=\"tab\"  target=\"_self\">Add/Edit Sequence</a></li>\n" +
-   "		</ul>\n" +
-   "		<div class=\"tab-content\">\n" +
-   "			<div class=\"tab-pane\" id=\"scratchpad\">\n" +
-   "				<div id=\"scratchpadContent\"></div>\n" +
-   "			</div>\n" +
-   "			<div class=\"tab-pane\" id=\"availableSequences\">\n" +
-   "				<div id=\"availableSequencesContent\"></div>\n" +
-   "			</div>\n" +
+WebHelpTemplates["WebHelpCreator"] = "<div id=\"webHelpMainContent\">\n" +
+   "    <div class=\"tabbable\">\n" +
+   "        <!-- Only required for left/right tabs -->\n" +
+   "        <ul class=\"nav nav-tabs\">\n" +
+   "            <li><a href=\"#addSequence\" data-toggle=\"tab\" target=\"_self\">Add/Edit Sequence</a></li>\n" +
+   "            <li><a href=\"#availableSequences\" data-toggle=\"tab\" target=\"_self\">Topics</a></li>\n" +
+   "            <li><a href=\"#scratchpad\" data-toggle=\"tab\" target=\"_self\">Scratchpad</a></li>\n" +
+   "        </ul>\n" +
+   "        <div class=\"tab-content\">\n" +
+   "            <div class=\"tab-pane\" id=\"scratchpad\">\n" +
+   "                <div id=\"scratchpadContent\"></div>\n" +
+   "            </div>\n" +
+   "            <div class=\"tab-pane\" id=\"availableSequences\">\n" +
+   "                <div id=\"availableSequencesContent\"></div>\n" +
+   "            </div>\n" +
    "\n" +
-   "			<div class=\"tab-pane\" id=\"addSequence\">\n" +
-   "				<div class=\"well\">\n" +
-   "\n" +
-   "					<section>\n" +
-   "						<div>\n" +
-   "							<input type=\"text\" id=\"sequenceTitleSetter\" placeholder=\"Sequence title\" />\n" +
-   "						</div>\n" +
-   "						<div id=\"stepsTable\" class=\"table table-bordered table-hover\"></div>\n" +
+   "            <div class=\"tab-pane\" id=\"addSequence\">\n" +
+   "                <div class=\"well\">\n" +
+   "                    <section>\n" +
+   "                        <div>\n" +
+   "                            <input type=\"text\" id=\"sequenceTitleSetter\" placeholder=\"Sequence title\"/>\n" +
+   "                        </div>\n" +
+   "                        <div id=\"stepsTable\" class=\"table table-bordered table-hover\"></div>\n" +
    "                    </section>\n" +
+   "                </div>\n" +
+   "                <div class=\"well \"><b>Create new steps:</b>\n" +
    "\n" +
-   "					<button type=\"button\" id='sequencePreviewButton' class=\"btn btn-default centered actionButton\" aria-label=\"Left Align\" style=\"margin-top:20px;\">\n" +
-   "						<span class=\"iconClass-play\" aria-hidden=\"true \"></span> Preview\n" +
-   "					</button>\n" +
+   "                    <div class=\"well-sm \">\n" +
+   "                        <button data-toggle=\"tooltip \" data-placement=\"top \"\n" +
+   "                                title=\"Click and drag over elements on the page to select them \"\n" +
+   "                                class=\"btn btn-success \" role=\"button \" id=\"startDragDropButton\"><span\n" +
+   "                                class=\"iconClass-add\"></span> Add element step\n" +
+   "                        </button>\n" +
+   "                        <button class=\"btn btn-info\" role=\"button\" id=\"startEmptyStepButton\">\n" +
+   "                            <span class=\"iconClass-add\"></span> Add page step\n" +
+   "                        </button>\n" +
+   "                        <button class=\"btn btn-danger \" id=\"cancelDragDropButton\" role=\"button \"><span\n" +
+   "                                class=\"iconClass-remove\"></span> Cancel\n" +
+   "                        </button>\n" +
+   "                    </div>\n" +
+   "                </div>\n" +
+   "                <div class=\"well\"><b>View or save sequence:</b>\n" +
+   "                    <div>\n" +
+   "                        <button type=\"button\" id='sequencePreviewButton' class=\"btn btn-default centered actionButton\"\n" +
+   "                                aria-label=\"Left Align\" style=\"margin-top:20px;\">\n" +
+   "                            <span class=\"iconClass-play\" aria-hidden=\"true \"></span> Preview\n" +
+   "                        </button>\n" +
+   "                        <button type=\"button \" id='sequenceSaveButton' class=\"btn btn-default centered \"\n" +
+   "                                aria-label=\"Left Align \" style=\"margin-top:20px;\">\n" +
+   "                            <span class=\"iconClass-save\" aria-hidden=\"true\"></span> Done\n" +
+   "                        </button>\n" +
+   "                        <button type=\"button\" id='clearStepsButton' class=\"btn btn-default centered\"\n" +
+   "                                aria-label=\"Left Align\"\n" +
+   "                                style=\"margin-top:20px;\">\n" +
+   "                            <span class=\"iconClass-clear\" aria-hidden=\"true \"></span> Clear\n" +
+   "                        </button>\n" +
+   "                    </div>\n" +
+   "                </div>\n" +
+   "                <div class=\"alert alert-danger\" id=\"noElementsSelectedDiv\" style=\"display: none;\">\n" +
+   "                    <button id=\"noElementsSelectedButton\" type=\"button\" class=\"close\" aria-label=\"Close\"><span\n" +
+   "                            aria-hidden=\"true\">&times;</span>\n" +
+   "                    </button>\n" +
+   "                    No elements were selected, please try again\n" +
+   "                </div>\n" +
    "\n" +
-   "					<button type=\"button \" id='sequenceSaveButton' class=\"btn btn-default centered \" aria-label=\"Left Align \" style=\"margin-top:20px;\">\n" +
-   "						<span class=\"iconClass-save\" aria-hidden=\"true\"></span> Done\n" +
-   "					</button>\n" +
-   "					<button type=\"button\" id='clearStepsButton' class=\"btn btn-default centered\" aria-label=\"Left Align\" style=\"margin-top:20px;\">\n" +
-   "						<span class=\"iconClass-clear\" aria-hidden=\"true \"></span> New\n" +
-   "					</button>\n" +
-   "				</div>\n" +
-   "\n" +
-   "				<div class=\"well \"><b>Available actions:</b>\n" +
-   "					<div class=\"well-sm \">\n" +
-   "						<button data-toggle=\"tooltip \" data-placement=\"top \" title=\"Click and drag over elements on the page to select them \" class=\"btn btn-success \" role=\"button \" id=\"startDragDropButton\"><span class=\"iconClass-add\"></span> Add element step\n" +
-   "						</button>\n" +
-   "						<button class=\"btn btn-info\" role=\"button\" id=\"startEmptyStepButton\">\n" +
-   "							<span class=\"iconClass-add\"></span> Add page step\n" +
-   "						</button>\n" +
-   "						<button class=\"btn btn-danger \" id=\"cancelDragDropButton\" role=\"button \"><span class=\"iconClass-remove\"></span> Cancel\n" +
-   "						</button>\n" +
-   "					</div>\n" +
-   "				</div>\n" +
-   "				<div class=\"alert alert-danger\" id=\"noElementsSelectedDiv\" style=\"display: none;\">\n" +
-   "					<button id=\"noElementsSelectedButton\" type=\"button\" class=\"close\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span>\n" +
-   "					</button>\n" +
-   "					No elements were selected, please try again\n" +
-   "				</div>\n" +
-   "\n" +
-   "				<div class=\"alert alert-danger\" id=\"noStepsInPreviewDiv\" style=\"display: none;\">\n" +
-   "					<button id=\"noStepsInPreviewButton\" type=\"button\" class=\"close\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span>\n" +
-   "					</button>\n" +
-   "					No steps to preview. Please add steps.\n" +
-   "				</div>\n" +
+   "                <div class=\"alert alert-danger\" id=\"noStepsInPreviewDiv\" style=\"display: none;\">\n" +
+   "                    <button id=\"noStepsInPreviewButton\" type=\"button\" class=\"close\" aria-label=\"Close\"><span\n" +
+   "                            aria-hidden=\"true\">&times;</span>\n" +
+   "                    </button>\n" +
+   "                    No steps to preview. Please add steps.\n" +
+   "                </div>\n" +
    "\n" +
    "                <div class=\"alert alert-info\" id=\"showSequenceSavedSuccessAlert\" style=\"display: none;\">\n" +
    "                    Sequence saved successfully!\n" +
    "                </div>\n" +
-   "			</div>\n" +
-   "		</div>\n" +
+   "            </div>\n" +
+   "        </div>\n" +
    "        <div class=\"well\" id=\"globalWebHelpCreatorActionsWell\"><b>Global actions:</b>\n" +
+   "\n" +
    "            <div class=\"well-sm \">\n" +
    "                <button class=\"btn btn-info\" role=\"button\" id=\"saveAllHelpSequencesToFileButton\">\n" +
    "                    <span class=\"iconClass-save\"></span> Save all sequences\n" +
    "                </button>\n" +
-   "                <button class=\"btn btn-success\" id=\"importAllHelpSequencesFromFileButton\" role=\"button \">\n" +
+   "                <button class=\"btn btn-success\" id=\"importAllHelpSequencesFromFileButton\" role=\"button\" title=\"Coming Soon\">\n" +
    "                    <span class=\"iconClass-upload\"></span> Import sequences from file\n" +
    "                </button>\n" +
    "            </div>\n" +
    "        </div>\n" +
    "\n" +
-   "	</div>\n" +
+   "    </div>\n" +
    "</div>\n" +
    "\n" +
    "\n" +
