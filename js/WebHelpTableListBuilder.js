1246c723d65de9e9a570f5c7063db74b589e384f
akannan <arj.shiv@gmail.com>
2015-10-27 12:28:18 -0400

    Start putting in JSDoc for TableList

--- js/WebHelpTableListBuilder.js
+++ js/WebHelpTableListBuilder.js
@@ -1,29 +1,46 @@
-
 /* globals jQuery, WebHelpTemplates */
+/**
+ * @namespace TableList
+ *
+ */
 var TableList;
 TableList = (function () {
 	"use strict";
-    function TableList(tableListOptions) {
-        if (tableListOptions.element === undefined) {
-            //console.error('TableList needs an element to work on');
-            throw new Error('TableList needs an element to work on');
-        }
-        var defaultOptions = {
-            element: '',
-            expandable: true,
-            searchable: true,
-            sortable: false,
-            emptyListIndicator: 'No data yet!',
-            data: [],
+	/**
+	 * Creates the TableList object with the specified settings
+	 * @param {Object} tableListOptions The configuration options for the TableList
+	 * @param {String} [tableListOptions.element] (DOM selector) The element on which to initialize the TableList
+	 * @param {String} [tableListOptions.emptyListIndicator='No data yet!'] What to show when you get an empty list
+	 * @param {String} [tableListOptions.listTemplate=WebHelpSequenceConsumptionList]
+	 * @param {String} [tableListOptions.listItemTemplate=WebHelpSequenceStepListItem]
+	 * @param {String} [tableListOptions.searchListTemplate=WebHelpTableListSearch]
+	 * @param {Boolean} [tableListOptions.expandable=true] Is expandable
+	 * @param {Boolean} [tableListOptions.searchable=true] Is searchable
+	 * @constructor [TableList]
+	 * @class TableList
+	 * @this TableList
+	 */
+	function TableList(tableListOptions) {
+		if (tableListOptions.element === undefined) {
+			//console.error('TableList needs an element to work on');
+			throw new Error('TableList needs an element to work on');
+		}
+		var defaultOptions = {
+			element: '',
+			expandable: true,
+			searchable: true,
+			sortable: false,
+			emptyListIndicator: 'No data yet!',
+			data: [],
 			status: "N",
-			seqId:null,
-            useData: true,
-            listTemplate: 'WebHelpSequenceConsumptionList',
-            listItemTemplate: 'WebHelpSequenceStepListItem',
-            searchListTemplate: 'WebHelpTableListSearch',
+			seqId: null,
+			useData: true,
+			listTemplate: 'WebHelpSequenceConsumptionList',
+			listItemTemplate: 'WebHelpSequenceStepListItem',
+			searchListTemplate: 'WebHelpTableListSearch',
 			supplementalClasses: []
-        };
-        for (var option in defaultOptions) {
+		};
+		for (var option in defaultOptions) {
 			if (!defaultOptions.hasOwnProperty(option)) {
 				continue;
 			}
@@ -43,7 +60,6 @@ TableList = (function () {
 		if (this.searchable) {
 			jQuery(this.element).children('.' + $searchListTemplate.attr('class')).remove();
 		}
-
 		var listItemTemplate = WebHelpTemplates[this.listItemTemplate];
 		if (this.useData) {
 			var length = this.data.length;
@@ -98,15 +114,12 @@ TableList = (function () {
 			.children('.' + $listTemplate.attr('class'))
 			.append($thisListItemTemplate);
 	};
-
 	TableList.prototype.removeRow = function (clickEvent) {
 		jQuery(clickEvent.target).parents('li.webHelpSequenceStepListItem').remove();
 	};
-
 	TableList.prototype.numRows = function () {
 		return jQuery(this.element).find('ul').find('li:not(.header)').length;
 	};
-
 	TableList.prototype.getData = function () {
 		/*No data binding, we have to evaluate this lazily*/
 		var listItems = jQuery(this.element).find('ul').find('li:not(.header):not(.webHelpEmptyListIndicator)');
@@ -125,47 +138,40 @@ TableList = (function () {
 		}
 		return returnArray;
 	};
-
 	TableList.prototype._searchFunction = function () {
 		var $listItems = jQuery(this.element).find('.' + jQuery(WebHelpTemplates[this.listTemplate]).attr('class')).find('li:not(.header)');
 		var $searchBox = jQuery(this.element).find('.' + jQuery(WebHelpTemplates[this.searchListTemplate]).attr('class')).find('input');
 		var val = $searchBox.val().trim();
 		var text;
-
 		$listItems.show().addClass('searchResults').filter(function () {
 			text = jQuery(this).text().replace(/\s+/g, ' ');
 			return !_fuzzySearch(text, val);
 		}).hide();
 	};
-
 	TableList.prototype.setData = function (givenData) {
 		this.data = givenData;
 	};
-
 	TableList.prototype.setStatus = function (status) {
 		this.status = status;
 	};
-
 	TableList.prototype.getStatus = function () {
 		return this.status;
 	};
-
 	TableList.prototype.setSeqId = function (seqId) {
 		this.seqId = seqId;
 	};
-
 	TableList.prototype.getSeqId = function () {
 		return this.seqId;
 	};
-
 	TableList.prototype._makeSortable = function () {
-		var $thisList = jQuery(this.element).find('.' + jQuery(WebHelpTemplates[this.listTemplate]).attr('class')); //Finds the list
+		var $thisList = jQuery(this.element).find('.' + jQuery(WebHelpTemplates[this.listTemplate]).attr('class')); //Finds
+	                                                                                                              // the
+	                                                                                                              // list
 		$thisList.sortable({
 			items: 'li:not(.header)',
 			cancel: 'div[contenteditable="true"], .fa'
 		});
 	};
-
 	//TODO Partial matching not complete
 	function _fuzzySearch(target, searchTerm) {
 		/*http://stackoverflow.com/a/15252131*/
