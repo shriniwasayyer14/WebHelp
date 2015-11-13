694aba85730869153fefa2a01ca7ed4fb09df5ed
saumyaj92 <saumyaj92@gmail.com>
2015-11-13 13:14:27 -0500

    Gulp changes

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+var gulp = require('gulp');
+var BowerWebpackPlugin = require('bower-webpack-plugin');
+var footer = require('gulp-footer');
+var jshint = require('gulp-jshint');
+var webpack = require('webpack');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var fileToJson = require('gulp-file-contents-to-json');
+var header = require('gulp-header');
+var gutil = require('gulp-util');
+var stylish = require('jshint-stylish');
+
+gulp.task('html2js', function(){
+    return gulp.src('templates/*.html')
+        .pipe(rename(function(moduleName){
+           moduleName.extname = '';
+         }))
+        .pipe(fileToJson('WebHelpTemplates.js'))
+        .pipe(header('var WebHelpTemplates ='))
+        .pipe(footer(';exports.WebHelpTemplates = WebHelpTemplates;'))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('stylus', function(){
+    return gulp.src('css/WebHelp.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('webpack', function(){
+    webpack({
+            entry:   './js/WebHelp.js',
+            output:  {
+                path:     __dirname + '/dist/js',
+                filename: 'AladdinHelp.js',
+                libraryTarget: 'var',
+                library: 'WebHelp'
+            },
+
+            module:  {
+                loaders: [
+                    {test: /\.css$/, loader: 'style!css'},
+                    {test: /\.styl$/, loader: 'style!css!stylus-loader'},
+                    {test: /\.less$/, loader: 'style!css!less'},
+                    {test: /\.(woff|svg|ttf|eot)([\?]?.*)$/, loader: 'file-loader?name=[name].[ext]'}
+                ]
+            },
+            plugins: [
+                new BowerWebpackPlugin({
+                    modulesDirectories: ['bower_components'],
+                    manifestFiles: 'bower.json',
+                    includes: /.*/,
+                    excludes: [],
+                    searchResolveModulesDirectories: true
+                    //excludes: /.*\.less/
+                }),
+                new webpack.ProvidePlugin({ //this creates globals inside the closure - only for requirements that return functions
+                    $:      'jquery',
+                    jQuery: 'jquery',
+                    introJsParent: 'intro.js'
+                })
+            ]
+        }, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+    });
+});
+
+gulp.task('jshint', function(){
+    return gulp.src(['gulpfile.js',
+        'js/**/*.js',
+        '!js/vendor/*.js',
+        'test/**/*.js'
+    ])
+        .pipe(jshint({
+
+        }))
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('concatJS', function(){
+    return gulp.src([
+        'bower_components/intro.js/intro.js',
+        'bower_components/jquery-ui/ui/core.js',
+        'bower_components/jquery-ui/ui/widget.js',
+        'bower_components/jquery-ui/ui/mouse.js',
+        'bower_components/jquery-ui/ui/position.js',
+        'bower_components/jquery-ui/ui/sortable.js',
+        'bower_components/jquery-get-path/dist/js/jQueryGetPath.js',
+        'js/vendor/*.js',
+        //'dist/bundle.js'
+        'js/*.js'
+    ])
+        .pipe(concat('AladdinHelp.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('concatCSS', ['stylus'], function(){
+   return gulp.src([
+       'bower_components/intro.js/minified/introjs.min.css',
+       'css/jQueryDragSelector.css',
+       'css/WebHelp.css'
+   ])
+       .pipe(concat('AladdinHelp.css'))
+       .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('concat', ['concatJS','concatCSS']);
+
+gulp.task('default', ['html2js','concatCSS','stylus','jshint','webpack']);
