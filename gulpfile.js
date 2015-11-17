d90f99309eba93a5eea0dc326b6b6949cd7a2265
saumyaj92 <saumyaj92@gmail.com>
2015-11-17 18:32:19 -0500

    Webpack minifies the built js file using UglifyjsPlugin

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,11 @@ var gutil = require('gulp-util');
 var stylish = require('jshint-stylish');
 var cssmin = require('gulp-cssmin');
 var uglify = require('gulp-uglify');
-
+//var path = require('path');
+var _ = require('lodash');
+//var resolveBowerPath = function(componentPath){
+//  return path.join(__dirname, 'bower_components', componentPath);
+//};
 gulp.task('html2js', function(){
     return gulp.src('templates/*.html')
         .pipe(rename(function(moduleName){
@@ -34,45 +38,68 @@ gulp.task('stylus', function(){
 });
 
 gulp.task('webpack', ['html2js'], function(){
-    webpack({
-            entry:   './js/WebHelp.js',
-            output:  {
-                path:     __dirname + '/dist/js',
-                filename: 'AladdinHelp.js',
-                libraryTarget: 'var',
-                library: 'WebHelp'
-            },
+    var unminifiedConfig = {
+        entry: './js/WebHelp.js',
+        output: {
+            path: __dirname + '/dist/js',
+            filename: 'AladdinHelp.js',
+            libraryTarget: 'var',
+            library: 'WebHelp'
+        }
+    };
 
-            module:  {
-                //loaders: [
-                //    {test: /\.css$/, loader: 'style!css'},
-                //    {test: /\.styl$/, loader: 'style!css!stylus-loader'},
-                //    {test: /\.less$/, loader: 'style!css!less'},
-                //    {test: /\.(woff|svg|ttf|eot)([\?]?.*)$/, loader: 'file-loader?name=[name].[ext]'}
-                //]
+    var minifiedConfig = _.cloneDeep(unminifiedConfig);
+    minifiedConfig.output.filename = 'AladdinHelp.min.js';
 
-            },
-            plugins: [
-                new BowerWebpackPlugin({
-                    modulesDirectories: ['bower_components'],
-                    manifestFiles: 'bower.json',
-                    includes: /.*/,
-                    excludes: [/.*\.less/, /.*\.css/],
-                    searchResolveModulesDirectories: true
-                    //excludes: /.*\.less/
-                }),
-                new webpack.ProvidePlugin({ //this creates globals inside the closure - only for requirements that return functions
-                    $:      'jquery',
-                    jQuery: 'jquery',
-                    introJsParent: 'intro.js'
-                })
-            ]
-        }, function(err, stats) {
+    //configure plugins
+    unminifiedConfig.plugins = getPlugins();
+    minifiedConfig.plugins = getPlugins(true);
+
+    /*Execute webpack*/
+    webpack(unminifiedConfig, function(err, stats) {
         if(err) { throw new gutil.PluginError('webpack', err);}
             gutil.log('[webpack]', stats.toString({
                 // output options
             }));
     });
+    webpack(minifiedConfig, function(err, stats) {
+        if(err) { throw new gutil.PluginError('webpack', err);}
+        gutil.log('[webpack]', stats.toString({
+            // output options
+        }));
+    });
+
+    function getPlugins(minify) {
+        var plugins = [
+            new BowerWebpackPlugin({
+                modulesDirectories: ['bower_components'],
+                manifestFiles: 'bower.json',
+                includes: /.*/,
+                excludes: [/.*\.less/, /.*\.css/],
+                searchResolveModulesDirectories: true
+                //excludes: /.*\.less/
+            }),
+            new webpack.ProvidePlugin({ //this creates globals inside the closure - only for requirements that return functions
+                $: 'jquery',
+                jQuery: 'jquery'
+            })
+        ];
+        if (minify) {
+            plugins.push(new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    sequences: true,
+                    dead_code: true,
+                    conditionals: true,
+                    booleans: true,
+                    unused: true,
+                    if_return: true,
+                    join_vars: true,
+                    drop_console: true
+                }
+            }));
+        }
+        return plugins;
+    }
 });
 
 gulp.task('jshint', function(){
@@ -145,4 +172,4 @@ gulp.task('uglify', ['webpack'], function(){
 });
 gulp.task('concat', ['concatJS','concatCSS']);
 
-gulp.task('default', ['html2js','stylus','concatCSS','jshint','cssmin','uglify','webpack']);
+gulp.task('default', ['html2js','stylus','concatCSS','jshint','cssmin','webpack']);
